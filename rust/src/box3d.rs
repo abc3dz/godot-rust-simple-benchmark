d44be2175c6496c9861718c5f5cf7ffc24499b66
ABC3Dz <archueyouler@gmail.com>
2026-05-05 20:18:16 +0700

    Testing StaticBody3D

--- rust/src/box3d.rs
+++ rust/src/box3d.rs
@@ -1,14 +1,14 @@
 use godot::prelude::*;
-use godot::classes::{Node3D};
+use godot::classes::{StaticBody3D, IStaticBody3D};
 
 #[derive(GodotClass)]
-#[class(init, base=Node3D)]
+#[class(init, base=StaticBody3D)]
 pub struct Box3d {
-    base: Base<Node3D>,
+    base: Base<StaticBody3D>,
 }
 
 #[godot_api]
-impl INode3D for Box3d {
+impl IStaticBody3D for Box3d {
     fn ready(&mut self) {
         // Initialization code for the box
     }
@@ -16,28 +16,4 @@ impl INode3D for Box3d {
 
 #[godot_api]
 impl Box3d {
-
-    // // #[func]
-    // // fn set_random_color(&mut self) {
-    // //     let mut rng = random_number_generator;
-
-    // //     let color = Color::from_rgb(
-    // //         rng.gen::<f32>(),
-    // //         rng.gen::<f32>(),
-    // //         rng.gen::<f32>(),
-    // //     );
-
-    // //     // หา MeshInstance3D
-    // //     let mesh = self
-    // //         .base()
-    // //         .get_node_as::<MeshInstance3D>("MeshInstance3D")
-    // //         .unwrap();
-
-    // //     // สร้าง material ใหม่
-    // //     let mut material = StandardMaterial3D::new_gd();
-    // //     material.set_albedo(color);
-
-    // //     // apply
-    // //     mesh.set_surface_override_material(0, material.upcast());
-    // // }
 }
--- rust/src/main_scene.rs
+++ rust/src/main_scene.rs
@@ -2,7 +2,7 @@ use crate::box3d;
 
 use godot::global::{randi_range};
 use godot::prelude::*;
-use godot::classes::{Node, PackedScene, InputEvent, Timer, Engine, Label};
+use godot::classes::{Node, PackedScene, Timer, Engine, Label};
 
 #[derive(GodotClass)]
 #[class(init, base=Node)]
@@ -25,61 +25,44 @@ pub struct MainScene {
 impl INode for MainScene {
     fn ready(&mut self) {
         self.spawned_count = 0;
+        self.spawn_box();
     }
-
-    // fn unhandled_input(&mut self, event: Gd<InputEvent>) {
-    //     if event.is_action_pressed("ui_accept"){
-    //         let scene = self.box_scene.as_ref().unwrap();
-
-    //         let mut box3d = scene
-    //             .instantiate_as::<box3d::Box3d>();
-    //         box3d.set_position(Vector3 { x: randi_range(-5, 5) as f32, y: randi_range(3, 5) as f32, z: randi_range(-5, 5) as f32 });
-            
-    //         self.base_mut().add_child(&box3d);
-    //     }
-    // }
 }
 
 #[godot_api]
 impl MainScene {
     fn spawn_box(&mut self) {
-        let width = 10;
-        let height = 8;
-        let spacing = 1.0;
-        
+        let spawn_amount = 5000; 
 
-        for y in 0..height {
-            for x in -5..width {
-                
-                let scene = self.box_scene.as_ref().unwrap();
-
-                let mut box3d = scene
-                    .instantiate_as::<box3d::Box3d>();
-
-                self.spawned_count += 80;
-
-                let pos = Vector3::new(
-                    x as f32 * spacing,
-                    y as f32 * spacing,
-                    0.0,
-                );
-
-                box3d.set_position(pos);
-
-                // เพิ่ม instance ใหม่เข้าไปใน Scene Tree
-                self.base_mut().add_child(&box3d); 
-        
-                
-        
+        let scene = match self.box_scene.as_ref() {
+            Some(s) => s.clone(),
+            None => {
+                godot_error!("Box scene not assigned!");
+                return;
             }
-        }
-        godot_print!("Spawned {} boxes!", &self.spawned_count);
+        };
 
+        for _ in 0..spawn_amount {
+            let mut box_instance = scene.instantiate_as::<box3d::Box3d>();
+
+            let pos = Vector3::new(
+                randi_range(-20, 20) as f32,
+                randi_range(10, 30) as f32,
+                randi_range(-20, 20) as f32,
+            );
+            box_instance.set_position(pos);
+
+            self.base_mut().add_child(&box_instance);
+            
+            self.spawned_count += 1;
+        }
+
+        // อัปเดต UI
         let fps = Engine::singleton().get_frames_per_second();
-        let text = format!("Rust\nFPS: {}\nSpawn Count: {}", fps, self.spawned_count);
+        let text = format!("Language: Rust\nFPS: {:.0}\nObjects: {}", fps, self.spawned_count);
         self.ui_label.set_text(&text);
-        
     }
+
     #[func]
     fn _on_spawn_timer_timeout(&mut self) {
         self.spawn_box();
