a54e503a5f6c6c3102c5517c3d599c147a301ccf
ABC3Dz <archueyouler@gmail.com>
2026-05-06 08:26:29 +0700

    add cal scene

--- godot/scenes/world/MainSceneCalCs.cs
+++ godot/scenes/world/MainSceneCalCs.cs
@@ -4,6 +4,7 @@ using System;
 public partial class MainSceneCalCs : Node
 {
     private Label uiLabel;
+    private float resultCount = 0;
 
     public override void _Ready()
     {
@@ -25,12 +26,19 @@ public partial class MainSceneCalCs : Node
             Vector3 v = new Vector3(Mathf.Sin(x), Mathf.Cos(x), Mathf.Tan(x));
             Vector3 n = v.Normalized();
             result += n.Length();
+            resultCount += result;
         }
+        
 
         ulong end = Time.GetTicksMsec();
         ulong duration = end - start;
+        double fps = Engine.GetFramesPerSecond();
 
-        string text = $"Language: C#\nTime: {duration} ms\nResult: {result}";
+        string text = $"Language: C#\nTime: {duration} ms\nResult: {resultCount}\nFPS: {fps:F0}";
         uiLabel.Text = text;
+    }
+    private void OnEventTimerTimeout()
+    {
+        RunBenchmark();
     }
 }
--- godot/scenes/world/main_scene_cal_gdsc.gd
+++ godot/scenes/world/main_scene_cal_gdsc.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @onready var ui_label = $CanvasLayer/Label
+var result_count = 0
 
 func _ready():
 	run_benchmark()
@@ -16,8 +17,15 @@ func run_benchmark():
 		var v = Vector3(sin(x), cos(x), tan(x))
 		var n = v.normalized()
 		result += n.length()
+		result_count += result
 
 	var end = Time.get_ticks_msec()
 	var duration = end - start
+	
+	var fps = Engine.get_frames_per_second()
 
-	ui_label.text = "Language: GDScript\nTime: %d ms\nResult: %f" % [duration, result]
+	ui_label.text = "Language: GDScript\nTime: %d ms\nResult: %f\nfps: %d" % [duration, result_count,fps]
+
+
+func _on_event_time_timeout() -> void:
+	run_benchmark()
